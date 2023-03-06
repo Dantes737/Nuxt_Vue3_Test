cc36131cc63da5d7d30cb704787b58c5666c27e5
Dantes737 <dantes.8ua8@gmail.com>
2023-03-06 19:22:23 +0200

    lol

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  nitro: {
-    output: {
-      dir: "output",
-      serverDir: "output/server",
-      publicDir: "output/public",
-    },
-  },
+  ssr: false,
   modules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
     // The private keys which are only available server-side
