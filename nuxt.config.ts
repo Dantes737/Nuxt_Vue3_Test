63ce413ef8b4b599b641029655a7f53967cdff73
Dantes737 <dantes.8ua8@gmail.com>
2023-03-06 18:28:05 +0200

    config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   modules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
     // The private keys which are only available server-side
